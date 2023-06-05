95764af30a90c8e49c69cd30da5f3f2d126a78ed
Maxim Manzik <kepkpkep@gmail.com>
2023-06-06 02:48:40 +0300

    account functionality and profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from django.utils.html import format_html
+
+from .models import Account, UserProfile
 
 
 # Register your models here.
@@ -14,3 +16,11 @@ class AccountAdmin(UserAdmin):
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html(f'<img scr="{object.profile_picture.url}" width="30" style="border-radius:50%;">')
+    thumbnail.short_description = 'Profile Picture'
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
